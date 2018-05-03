af448fa5b78a361c1df9fc8efbee7192898ea54e
Rani Zilpelwar <rani@8thlight.com>
2018-05-03 15:06:56 -0400

    Refactored responses to use data from request body json instead of params

--- tic_tac_toe_web.rb
+++ tic_tac_toe_web.rb
@@ -4,7 +4,12 @@ require 'tic_tac_toe_rz'
 require 'json'
 
 before do
-	headers 'Content-Type' => 'json'
+	headers 'Content-Type' => 'application/json'
+  if request.body.size > 0
+    request.body.rewind
+    @request_data = JSON.parse request.body.read
+    puts "request data: #{@request_data}"
+  end
 end
 
 get '/match_types' do
@@ -14,6 +19,7 @@ get '/match_types' do
       {"player1_type": match_manager.player_type(2,1), "player2_type": match_manager.player_type(2,2)},
       {"player1_type": match_manager.player_type(3,1), "player2_type": match_manager.player_type(3,2)}
       ]}
+  puts "response data = #{data}"
   data.to_json
 end
 
@@ -23,65 +29,66 @@ end
 post '/game' do
   #language_adapter = TicTacToeRZ::LanguageOptionsAdapter.new(directory)
   language_tag = "en"
-  match_number = params[:match_number].to_i
+  match_number = @request_data['match_number'].to_i
   match_manager = TicTacToeRZ::MatchTypeManager.new
-  player1_symbol = params[:first_player_symbol]
-  player2_symbol = params[:second_player_symbol]
+  player1_symbol = @request_data['first_player_symbol']
+  player2_symbol = @request_data['second_player_symbol']
   player1 = TicTacToeRZ::Player.new(match_manager.player_type(match_number,1), player1_symbol)
   player2 = TicTacToeRZ::Player.new(match_manager.player_type(match_number,2), player2_symbol)
   player_manager = TicTacToeRZ::PlayerManager.new(player1, player2)
   game_board = TicTacToeRZ::GameBoard.new(TicTacToeRZ::GameBoard.create_board)
   player_movement_manager = TicTacToeRZ::PlayerMovementManager.new(match_manager.get_match_type(match_number))
-
-  game = { "language_tag": language_tag,
-           "match_number": match_number,
-           "player1_symbol": player1_symbol, 
-           "player2_symbol": player2_symbol,
-           "current_player_symbol": player_manager.current_player.symbol,
-           "board": game_board.board,
-           "record_moves": player_movement_manager.moves_recordable?(match_number)
-  }
-  game.to_json
+  data = { "game": {
+         "language_tag": language_tag,
+         "match_number": match_number,
+         "player1_symbol": player1_symbol, 
+         "player2_symbol": player2_symbol,
+         "current_player_symbol": player_manager.current_player.symbol,
+         "board": game_board.board,
+         "record_moves": player_movement_manager.moves_recordable?(match_number)}
+       }
+  puts "response data = #{data}"
+  data.to_json
 end
 
 put '/human_players_turn' do
-  current_player_symbol = params[:current_player_symbol]
-  tile_on_board = params[:tile_on_board]
-  board = params[:board]
+  current_player_symbol = @request_data['game']['current_player_symbol']
+  tile_on_board = @request_data['actions']['tile_on_board']
+  board = @request_data['game']['board']
   game_board = TicTacToeRZ::GameBoard.new(board)
   return_result = TicTacToeRZ::GamePlayValidator.evaluate_move(game_board, tile_on_board)
   valid_move = return_result.is_valid_move
   spot = return_result.index_of_board
   game = {}
   success = false
+  puts "valid_move = #{valid_move}"
   if !valid_move
     data = { "game": {
-           "language_tag": params[:language_tag],
-           "match_number": params[:match_number],
-           "player1_symbol": params[:player1_symbol], 
-           "player2_symbol": params[:player2_symbol],
-           "current_player_symbol": params[:current_player_symbol],
+           "language_tag": @request_data['game']['language_tag'],
+           "match_number": @request_data['game']['match_number'],
+           "player1_symbol": @request_data['game']['player1_symbol'], 
+           "player2_symbol": @request_data['game']['player2_symbol'],
+           "current_player_symbol": @request_data['game']['current_player_symbol'],
            "board": board,
-           "record_moves": params[:record_moves]
-         },
-         "success?": success
+           "record_moves": @request_data['game']['record_moves']
+         }
     }
   else
-    record_last_move(current_player_symbol, spot) if params[:record_moves]
+    record_last_move(current_player_symbol, spot) if @request_data['record_moves']
     game_board.update_board(spot.to_i, current_player_symbol)
     success = true
     data = { "game": {
-           "language_tag": params[:language_tag],
-           "match_number": params[:match_number],
-           "player1_symbol": params[:player1_symbol], 
-           "player2_symbol": params[:player2_symbol],
-           "current_player_symbol": params[:current_player_symbol],
+           "language_tag": @request_data['game']['language_tag'],
+           "match_number": @request_data['game']['match_number'],
+           "player1_symbol": @request_data['game']['player1_symbol'], 
+           "player2_symbol": @request_data['game']['player2_symbol'],
+           "current_player_symbol": @request_data['game']['current_player_symbol'],
            "board": game_board.board,
-           "record_moves": params[:record_moves]
-           },
-          "success?": success
+           "record_moves": @request_data['game']['record_moves']
+           }
     }
   end
+  puts "response data = #{data}"
   data.to_json
 end
 
