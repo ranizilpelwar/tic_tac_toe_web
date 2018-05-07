b1822f09280abd745fc5ab9a900b8ae7c23bd11c
Rani Zilpelwar <rani@8thlight.com>
2018-05-07 13:34:50 -0400

    add convenience method to extract entire game

--- data/data_parser.rb
+++ data/data_parser.rb
@@ -10,4 +10,21 @@ module DataParser
       request_data[property_name]
     end
   end
+
+  def self.parse_game(request_data)
+    game = {}
+    game[:current_player_symbol] = parse(request_data, 'game', 'current_player_symbol')
+    game[:tile_on_board] = parse(request_data, 'actions','tile_on_board')
+    game[:board] = parse(request_data, 'game','board')
+    game[:language_tag] = parse(request_data, 'game','language_tag')
+    game[:match_number] = parse(request_data, 'game','match_number')
+    game[:player1_symbol] = parse(request_data, 'game','player1_symbol')
+    game[:player2_symbol] = parse(request_data, 'game','player2_symbol')
+    game[:current_player_symbol] = parse(request_data, 'game', 'current_player_symbol')
+    game[:last_move_for_player1] = parse(request_data, 'game', 'last_move_for_player1')
+    game[:last_move_for_player2] = parse(request_data, 'game', 'last_move_for_player2')
+    game[:record_moves] = parse(request_data, 'game', 'record_moves')
+    game[:error_message] = ""
+    game
+  end
 end
