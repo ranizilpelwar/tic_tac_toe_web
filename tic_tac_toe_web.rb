d929f0f2e6a6ae942093ca415e61a5b585d058d7
Rani Zilpelwar <rani@8thlight.com>
2018-05-08 10:53:46 -0400

    Add undo_move method

--- tic_tac_toe_web.rb
+++ tic_tac_toe_web.rb
@@ -176,3 +176,34 @@ put '/computer_players_turn' do
   end
   ResponseGenerator.generate_game(game)
 end
+
+put '/undo_move' do
+  game = {}
+  begin
+    game = DataParser.parse_game(@request_data)
+  rescue SyntaxError, NoMethodError, TicTacToeRZ::NilReferenceError => error
+    game[:error_message] = "#{ error.class.name }: #{ error.message }"
+    status 400
+  else
+    begin
+      match_manager = TicTacToeRZ::MatchTypeManager.new
+      match_number = game[:match_number]
+      player1 = TicTacToeRZ::Player.new(match_manager.player_type(match_number,1), game[:player1_symbol])
+      player2 = TicTacToeRZ::Player.new(match_manager.player_type(match_number,2), game[:player2_symbol])
+      player_manager = TicTacToeRZ::PlayerManager.new(player1, player2)
+      current_player_symbol = game[:current_player_symbol]
+      player_manager.update_current_player if current_player_symbol == player2.symbol
+      game_board = TicTacToeRZ::GameBoard.new(game[:board])
+      player_movement_manager = TicTacToeRZ::PlayerMovementManager.new(match_manager.get_match_type(match_number))
+      player_movement_manager.update_last_move_for_player(1, game[:last_move_for_player1])
+      player_movement_manager.update_last_move_for_player(2, game[:last_move_for_player2])
+      player_movement_manager.undo_last_move(game_board, player_manager)
+      game[:board] = game_board.board
+    rescue TicTacToeRZ::InvalidValueError, TicTacToeRZ::NilReferenceError => error
+      game[:error_message] = "#{ error.class.name }: #{ error.message }"
+      game[:stack_trace] = "#{ error.backtrace.join("\n") }"
+      status 400
+    end
+  end
+  ResponseGenerator.generate_game(game)
+end
