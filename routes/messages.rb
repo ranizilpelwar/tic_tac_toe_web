d36d36c1e23652d72162c17adc3e57b8be812e6e
Rani Zilpelwar <rani@8thlight.com>
2018-05-09 16:20:31 -0400

    text will be empty string if there are errors

--- routes/messages.rb
+++ routes/messages.rb
@@ -12,12 +12,13 @@ put '/message_content' do
     language_adapter.default_language_tag!(data[:language_tag])
     type = data[:type]
     method = type.to_sym
-    data[:text] = TicTacToeRZ::MessageGenerator.send method
+    text = TicTacToeRZ::MessageGenerator.send method
   rescue NoMethodError, TicTacToeRZ::InvalidValueError => error
     error_message = "#{ error.class.name }: #{ error.message }"
     data[:error_message] = error_message
     status 400
   end
+  data[:text] = text.nil? ? "" : text
   ResponseGenerator.generate_message(data)
 end
 
