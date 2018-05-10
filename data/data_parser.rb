0a2076c93d8d8df1ea3e29a2b61a085b316519f6
Rani Zilpelwar <rani@8thlight.com>
2018-05-10 09:41:27 -0400

    Clean up error message to remove English wording and standardize the info provided

--- data/data_parser.rb
+++ data/data_parser.rb
@@ -3,10 +3,10 @@ require 'tic_tac_toe_rz'
 module DataParser
   def self.parse(request_data, category, property_name)
     if !category.nil?
-      raise TicTacToeRZ::NilReferenceError, "#{property_name} in #{category} is missing" if !request_data[category].include?(property_name)
+      raise TicTacToeRZ::NilReferenceError, "category: #{category}, property: #{property_name}" if !request_data[category].include?(property_name)
       request_data[category][property_name]
     else
-      raise TicTacToeRZ::NilReferenceError, "#{property_name} is missing" if !request_data.include?(property_name)
+      raise TicTacToeRZ::NilReferenceError, "property: #{property_name}" if !request_data.include?(property_name)
       request_data[property_name]
     end
   end
