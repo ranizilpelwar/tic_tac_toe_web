ab7c32bc79d3bbb51e4d45b5ca94094c13c29ae9
Rani Zilpelwar <rani@8thlight.com>
2018-05-11 16:04:02 -0400

    Separate schema logic out of controller and into model class

---
+++ models/game_status.rb
@@ -0,0 +1,43 @@
+require 'tic_tac_toe_rz'
+require_relative '../helpers/object_creator.rb'
+
+module Models
+  class GameStatus
+
+    attr_accessor :error_message
+
+    def initialize
+      @tie_game = false
+      @game_over = false
+      @winner = ""
+      @error_message = ""
+    end
+
+    def parse(request_data)
+      @board = DataParser.parse(request_data, 'game', 'board')
+      @player1_symbol = DataParser.parse(request_data, 'game', 'player1_symbol')
+      @player2_symbol = DataParser.parse(request_data, 'game', 'player2_symbol')
+    end
+
+    def construct
+      if TicTacToeRZ::TieGameValidator.tie_game?(@board)
+        @game_over = true
+        @tie_game = true
+      elsif TicTacToeRZ::GameOverValidator.win_for_player?(@player1_symbol, @board)
+        @game_over = true
+        @winner = @player1_symbol
+      elsif TicTacToeRZ::GameOverValidator.win_for_player?(@player2_symbol, @board)
+        @game_over = true
+        @winner = @player2_symbol
+      end
+    end
+
+    def content
+      data = {:error_message => @error_message,
+              :tie_game => @tie_game, 
+              :game_over => @game_over, 
+              :winner => @winner
+             }
+    end
+  end
+end
--- routes/game_statuses.rb
+++ routes/game_statuses.rb
@@ -2,34 +2,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tic_tac_toe_rz'
 require 'json'
-require_relative '../data/data_parser.rb'
 require_relative '../response/response_generator.rb'
+require_relative '../models/game_status.rb'
 
 put '/game_status' do
-  error_message = ""
-  tie_game = false
-  game_over = false
-  winner = ""
+  game_status = Models::GameStatus.new
   begin
-    board = DataParser.parse(@request_data, 'game', 'board')
-    player1_symbol = DataParser.parse(@request_data, 'game', 'player1_symbol')
-    player2_symbol = DataParser.parse(@request_data, 'game', 'player2_symbol')
+    game_status.parse(@request_data)
   rescue SyntaxError, NoMethodError, TicTacToeRZ::NilReferenceError => error
-    error_message = "#{ error.class.name }: #{ error.message }"
+    game_status.error_message = "#{ error.class.name }: #{ error.message }"
     status 400
   else
-    if TicTacToeRZ::TieGameValidator.tie_game?(board)
-      game_over = true
-      tie_game = true
-    elsif TicTacToeRZ::GameOverValidator.win_for_player?(player1_symbol, board)
-      game_over = true
-      winner = player1_symbol
-    elsif TicTacToeRZ::GameOverValidator.win_for_player?(player2_symbol, board)
-      game_over = true
-      winner = player2_symbol
-    end
+    game_status.construct
   end
-  data = {:error_message => error_message, :tie_game => tie_game, 
-          :game_over => game_over, :winner => winner}
-  ResponseGenerator.generate_game_status(data)
+  ResponseGenerator.generate_game_status(game_status.content)
 end
