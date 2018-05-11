50ba8457b497a98563e1ab2d70ba2d4c6fb60a0c
Rani Zilpelwar <rani@8thlight.com>
2018-05-11 12:21:27 -0400

    reorder fields just to improve flow of info

--- data/data_parser.rb
+++ data/data_parser.rb
@@ -3,7 +3,8 @@ require 'tic_tac_toe_rz'
 module DataParser
   def self.parse(request_data, category, property_name)
     if !category.nil?
-      raise TicTacToeRZ::NilReferenceError, "category: #{category}, property: #{property_name}" if request_data[category].nil? || !request_data[category].include?(property_name)
+      raise TicTacToeRZ::NilReferenceError, "category: #{category}, property: #{property_name}" if request_data[category].nil? 
+      raise TicTacToeRZ::NilReferenceError, "category: #{category}, property: #{property_name}" if !request_data[category].include?(property_name)
       request_data[category][property_name]
     else
       raise TicTacToeRZ::NilReferenceError, "property: #{property_name}" if !request_data.include?(property_name)
@@ -13,12 +14,12 @@ module DataParser
 
   def self.parse_game(request_data)
     game = {}
-    game[:board] = parse(request_data, 'game','board')
     game[:language_tag] = parse(request_data, 'game','language_tag')
     game[:match_number] = parse(request_data, 'game','match_number')
     game[:player1_symbol] = parse(request_data, 'game','player1_symbol')
     game[:player2_symbol] = parse(request_data, 'game','player2_symbol')
     game[:current_player_symbol] = parse(request_data, 'game', 'current_player_symbol')
+    game[:board] = parse(request_data, 'game','board')
     game[:last_move_for_player1] = parse(request_data, 'game', 'last_move_for_player1')
     game[:last_move_for_player2] = parse(request_data, 'game', 'last_move_for_player2')
     game[:record_moves] = parse(request_data, 'game', 'record_moves')
