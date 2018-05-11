77fdfb015407c40ea86d525d3d9d8b3687fd93b1
Rani Zilpelwar <rani@8thlight.com>
2018-05-11 15:35:20 -0400

    Use the new match_types model

--- routes/matches.rb
+++ routes/matches.rb
@@ -3,19 +3,15 @@ require 'sinatra/reloader'
 require 'tic_tac_toe_rz'
 require 'json'
 require_relative '../response/response_generator.rb'
+require_relative '../models/match_types.rb'
 
 get '/match_types' do
-  error_message = ""
-  match_manager = TicTacToeRZ::MatchTypeManager.new
   begin
-    data = {:match1_player1_type => match_manager.player_type(1,1), :match1_player2_type => match_manager.player_type(1,2),
-        :match2_player1_type => match_manager.player_type(2,1), :match2_player2_type => match_manager.player_type(2,2),
-        :match3_player1_type => match_manager.player_type(3,1), :match3_player2_type => match_manager.player_type(3,2),
-        :error_message => error_message}
+    match_types = Models::MatchTypes.new
+    match_types.construct
   rescue TicTacToeRZ::InvalidValueError => error
-    error_message = "#{ error.class.name }: #{ error.message }"
-    data[:error_message] = error
+    match_types.error_message = "#{ error.class.name }: #{ error.message }"
     status 400
   end
-  ResponseGenerator.generate_matches(data)
+  ResponseGenerator.generate_matches(match_types.content)
 end
