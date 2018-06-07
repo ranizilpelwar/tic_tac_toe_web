07e916b8143b459b41081503b9ae3334d4fac1be
Rani Zilpelwar <rani@8thlight.com>
2018-06-07 09:58:49 -0400

    Utilize duck type for retrieving list of languages available to play the game

--- models/language.rb
+++ models/language.rb
@@ -7,8 +7,22 @@ module Models
     attr_reader :language_tag
 
     def initialize
-      language_adapter = ObjectCreator.language_adapter
-      @language_tag = language_adapter.default_language_tag
+      @languages = {}
+      @language_adapter = ObjectCreator.language_adapter
+      @language_tag = @language_adapter.default_language_tag
+    end
+
+    def construct
+      @languages = @language_adapter.all_languages
+    end
+
+    def content
+      convert_to_symbols = []
+      @languages.each do |language|
+        record = {:description => language["description"], :language_tag => language["tag"]}
+        convert_to_symbols << record
+      end
+      convert_to_symbols
     end
   end
 end
